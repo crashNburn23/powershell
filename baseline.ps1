3bb0f3e61525f62fd4550bc1124179b46c37c760
darrell lytle <darrell.lytle@gmail.com>
2015-07-19 21:20:35 -0400

    new switch

--- baseline.ps1
+++ baseline.ps1
@@ -1,18 +1,21 @@
-## This script should be modified for use on multiple computers (i.e. define a variable ($computers with all computer names), foreach($computer in $computers), Invoke-Command -computername $computer script-block{ }
-## Additionally baselining should not be limited to what I put in here, it should be tailored and intel driven
-
 [cmdletbinding()]
-param([switch]$help,[switch]$baseline,[switch]$compare,[switch]$adcomputer)
+param([switch]$baseline,[switch]$compare,[switch]$h,[switch]$adcomputer)
 if($baseline) {
     Get-Service | Export-Clixml c:\services.xml
     Get-Process | Export-Clixml c:\process.xml
-    Write-Host "      Output is located at c:\services.xml and c:\process.xml" -ForegroundColor Green}
-Elseif($compare) {
-    Compare-Object -ReferenceObject(Import-Clixml c:\services.xml) -DifferenceObject(get-service) -Property name, status | Out-File c:\servicelog.txt
-    Compare-Object -ReferenceObject(import-clixml c:\process.xml) -DifferenceObject(get-process) -Property name | Out-file c:\processlog.txt
-    Write-Host "      Output is located at c:\servicelog.txt and c:\processlog.txt" -ForegroundColor Green}
-Elseif($adcomputer) {
-    Get-Adcomputer -filter * | Select-Object -Property name | ft -HideTableHeaders | Export-Clixml c:\computers.xml
-    Write-Host "      Output is located at c:\computers.xml" -ForegroundColor Green}
-Else{
-    Write-Host "use -h for switch options" -ForegroundColor yellow }
+    Write-Host "    Output is located at c:\process.xml and c:\services.xml" -ForegroundColor Green}
+elseif($compare) {
+    Compare-Object -ReferenceObject(Import-Clixml C:\services.xml) -DifferenceObject(Get-Service) -Property name, status | Out-File C:\servicelog.txt
+    Compare-Object -ReferenceObject(Import-Clixml C:\process.xml) -DifferenceObject(Get-Process) -Property name | Out-File C:\processlog.txt
+    Write-Host "    Output is located at c:\servicelog.txt and c:\processlog.txt" -ForegroundColor Green}
+elseif($h) {
+    Write-Host " "
+    Write-Host "    Options:" 
+    Write-Host "        -baseline:     Conducts Service and Process List Baseline"
+    Write-Host "        -compare:      Conducts a baseline comparison" 
+    Write-Host "        -adcomputer    Creates an .xml file of all computers in AD (Requires ActiveDirectory Modules!)"}
+elseif($adcomputer) {
+    Get-ADComputer -filter * | Select-Object -property name | ft -HideTableHeaders | Export-Clixml c:\computers.xml
+    Write-Host "    Output is located at c:\computers.xml" -ForegroundColor Green}
+else{
+    Write-Host "use -h for switch options" -ForegroundColor Yellow}
