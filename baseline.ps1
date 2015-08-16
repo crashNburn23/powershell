2fd768f8afce24a9b8ec6a1547839de529077b5b
lytledw <darrell.lytle@gmail.com>
2015-08-16 16:38:03 -0400

    Tee-Object added

--- baseline.ps1
+++ baseline.ps1
@@ -3,28 +3,25 @@ param([switch]$baseline,[switch]$compare,[switch]$h,[switch]$adcomputer)
 if($baseline) {
     Get-Service | Export-Clixml c:\tools\services.xml
     Get-Process | Export-Clixml c:\tools\process.xml
-    Write-Host "    Output is located at c:\tools\process.xml and c:\tools\services.xml" -ForegroundColor Green}
+    Write-Host `r`n "    Output is located at c:\tools\process.xml and c:\tools\services.xml" -ForegroundColor Green
+    }
 elseif($compare) {
-    Compare-Object -ReferenceObject(Import-Clixml C:\tools\services.xml) -DifferenceObject(Get-Service) -Property name, status | Out-File C:\tools\servicelog.txt
-    Compare-Object -ReferenceObject(Import-Clixml C:\tools\process.xml) -DifferenceObject(Get-Process) -Property name | Out-File C:\tools\processlog.txt
-    $a= Compare-Object -ReferenceObject(Import-Clixml C:\tools\services.xml) -DifferenceObject(Get-Service) -Property name, status 
-    $b= Compare-Object -ReferenceObject(Import-Clixml C:\tools\process.xml) -DifferenceObject(Get-Process) -Property name
-    Write-Host "    Output is located at c:\tools\servicelog.txt and c:\tools\processlog.txt" -ForegroundColor Green
-    Write-Host "Service changes are below:" -ForegroundColor Cyan
-    Write-Output $a 
-    Write-Host "Process changes are below:" -ForegroundColor Cyan
-    Write-Output $b 
-   }
+    Write-Host `r`n "    Differences are listed below:" -ForegroundColor Cyan
+    Compare-Object -ReferenceObject(Import-Clixml C:\tools\services.xml) -DifferenceObject(Get-Service) -Property name, status | Tee-Object -FilePath C:\tools\servicelog.txt
+    Compare-Object -ReferenceObject(Import-Clixml C:\tools\process.xml) -DifferenceObject(Get-Process) -Property name | Tee-Object -FilePath C:\tools\processlog.txt
+    Write-Host `r`n "    Output is stored at c:\tools\servicelog.txt and c:\tools\processlog.txt" -ForegroundColor Green
+    }
 elseif($h) {
-    Write-Host " "
-    Write-Host "    Options:" 
-    Write-Host "        -baseline:     Conducts Service and Process List Baseline"
-    Write-Host "        -compare:      Conducts a baseline comparison" 
-    Write-Host "        -adcomputer    Creates an .xml file of all computers in AD (Requires ActiveDirectory Modules!)"}
+    Write-Host " Options:" -ForegroundColor Yellow
+    Write-Host " -baseline:" `t "Conducts Service and Process List Baseline" -ForegroundColor Yellow
+    Write-Host " -compare:" `t`t "Conducts a baseline comparison" -ForegroundColor Yellow
+    Write-Host " -adcomputer:" `t "Creates an .xml file of all computers in AD (Requires ActiveDirectory Modules!)" -ForegroundColor Yellow} 
 elseif($adcomputer) {
     Get-ADComputer -filter * | Select-Object -property name | ft -HideTableHeaders | Export-Clixml c:\tools\computers.xml
-    Write-Host "    Output is located at c:\tools\computers.xml" -ForegroundColor Green}
+    Write-Host "    Output is located at c:\tools\computers.xml" -ForegroundColor Green
+    }
 else{
-    Write-Host "use -h for switch options" -ForegroundColor Yellow}
+    Write-Host "use -h for switch options" -ForegroundColor Yellow
+    }
 
    
